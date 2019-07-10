63a264d3e8229eafe44e9a6cf316ad0d0919c320
Paul <paul@Pauls-MacBook-Pro.local>
2019-07-10 19:38:41 -0400

    Helpdesk tools

---
+++ HelpDesk Tools/hideHidenFiles.sh
@@ -0,0 +1,1 @@
+defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
---
+++ HelpDesk Tools/showHiddenFiles.sh
@@ -0,0 +1,1 @@
+defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app
