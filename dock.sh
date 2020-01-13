4a4c452c2be86abc83e8b8f3fb773998de1ac6d5
TheNite <paul@noxian.net>
2020-01-13 09:03:32 -0500

    updated dock script for catalina

--- dock.sh
+++ dock.sh
@@ -11,17 +11,21 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 # Apps that are installed in the '/Applications/' directory
-Apps=("Firefox.app" "Slack.app" "Sublime Text.app" "1Password 7.app" "Messages.app" "System Preferences.app" "Github Desktop.app" "PostMan.app" "xcode.app" "App store.app")
+Apps=("Firefox.app" "Slack.app" "Sublime Text.app" "1Password 7.app" "Messages.app" "Github Desktop.app" "PostMan.app" "xcode.app" "App store.app")
 
-#Apps that are installed in the '/Applications/Utitlies/' directory
-utilApps=("Terminal.app")
+#Default systems that are installed in the '/Applications/Utitlies/' directory
+system_utilities_apps=("Terminal.app")
+
+#System defautl apps that are installed in the '/Applications/' OR for Catalina in '/System/Applications/' directory
+system_default_apps=("System Preferences.app")
 
 #Folders to add to the dock
-userFolders=("Downloads")
+user_folders=("Downloads")
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # Variables
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+ macos_vers=$(sw_vers -productVersion | awk -F "." '{print $2}')
 
 dockutil=/usr/local/bin/dockutil
 sleepTime=2
@@ -37,20 +41,49 @@ $dockutil --remove all --no-restart --allhomes
 # Add apps to docks
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-for app in "${Apps[@]}"; do
-	echo ${app}
-	$dockutil --add "$appFolder/${app}" --no-restart --allhomes
-done
+function dockutil_catalina {
+	for app in "${Apps[@]}"; do
+		echo ${app}
+		$dockutil --add "$appFolder/${app}" --no-restart --allhomes
+	done
 
-for app in "${utilApps[@]}"; do
-	$dockutil --add "$utilAppFolder/${app}" --no-restart --allhomes
-done
+	for app in "${system_default_apps[@]}"; do
+		echo ${app}
+		$dockutil --add "/System/Applications/${app}" --no-restart --allhomes
+	done
 
-for folder in "${userFolders[@]}"; do
-	$dockutil --add "~/${folder}" --no-restart --allhomes
-done
+	for app in "${system_utilities_apps[@]}"; do
+		echo ${app}
+		$dockutil --add "/System/$utilAppFolder/${app}" --no-restart --allhomes
+	done
+
+	for folder in "${userFolders[@]}"; do
+		$dockutil --add "~/${folder}" --no-restart --allhomes
+	done
+}
+
+function dockutil_all() {
+	for app in "${Apps[@]}"; do
+		echo ${app}
+		$dockutil --add "$appFolder/${app}" --no-restart --allhomes
+	done
+
+	for app in "${system_utilities_apps[@]}"; do
+		$dockutil --add "$utilAppFolder/${app}" --no-restart --allhomes
+	done
+
+	for folder in "${userFolders[@]}"; do
+		$dockutil --add "~/${folder}" --no-restart --allhomes
+	done
+}
 
 # Cleanup
+
+if [[ "$macos_vers" -ge 15 ]]; then
+	dockutil_catalina
+else
+	dockutil_all
+fi
 
 sleep $sleepTime
 
